e95ac666c2d54f564d0fbd8d74b48bef77b2d351
Fadil <fadilah.mikroj@gmail.com>
2024-05-24 21:01:17 +0700

    Change MainActivity.kt

--- app/src/main/java/com/example/e_iqra/MainActivity.kt
+++ app/src/main/java/com/example/e_iqra/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        println("Hello !!!")
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
