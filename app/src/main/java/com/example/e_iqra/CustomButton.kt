c5a37c5432596784f3a7f85953bcdaacd1311466
Xtrant <95553498+Xtrant@users.noreply.github.com>
2024-05-25 15:15:13 +0700

    add a design of login page

---
+++ app/src/main/java/com/example/e_iqra/CustomButton.kt
@@ -0,0 +1,33 @@
+package com.example.e_iqra
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+
+class CustomButton : AppCompatButton {
+    constructor(context : Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    private var txtColor: Int = 0
+    private var enabledBackground: Drawable
+    private var disabledBackground: Drawable
+
+    init {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_dark)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = if (isEnabled) enabledBackground else disabledBackground
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = if (isEnabled) "Login" else "Fill First"
+    }
+}
