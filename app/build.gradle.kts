b512ce41b097a849fd9dde60298f6be71e9c4edd
Fadil <fadilah.mikroj@gmail.com>
2024-05-25 15:00:57 +0700

    - Created a new activity called "WelcomeActivity" using MotionLayout to showcase application features. - Added settings to "WelcomeActivity" to ensure it is only displayed when the application is first run or when the user re-downloads and opens the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/e_iqra/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/e_iqra/view/welcome/WelcomeActivity.kt
@@ -1,21 +1,29 @@
 package com.example.e_iqra.view.welcome
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.e_iqra.MainActivity
 import com.example.e_iqra.R
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val sharedPreferences = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("isFirstTime", true)
+
+        if (isFirstTime) {
+            setContentView(R.layout.activity_welcome)
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("isFirstTime", false)
+            editor.apply()
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
+
+
