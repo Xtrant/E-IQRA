e9533a4eb0085b892591b9083a0df834ca6e1cea
Xtrant <95553498+Xtrant@users.noreply.github.com>
2024-06-03 16:06:37 +0700

    Edit Canvas Fragment to Add gallery Access

--- app/src/main/java/com/example/e_iqra/view/main/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/e_iqra/view/main/ui/dashboard/DashboardFragment.kt
@@ -1,11 +1,15 @@
 package com.example.e_iqra.view.main.ui.dashboard
 
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.e_iqra.databinding.FragmentDashboardBinding
@@ -15,6 +19,7 @@ class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+    private var currentImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,8 +45,46 @@ class DashboardFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGallery.setOnClickListener {
+            openGallery()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun openGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.ivResult.setImageURI(it)
+            settingResultImage()
+        }
+    }
+
+    private fun settingResultImage() {
+        if (currentImageUri != null) {
+            binding.canvasButton.visibility = View.GONE
+            binding.ivResult.visibility = View.VISIBLE
+        }
+
     }
 }
